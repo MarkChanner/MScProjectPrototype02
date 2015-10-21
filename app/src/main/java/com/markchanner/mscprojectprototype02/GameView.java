c72e510e3af81df4649cd363a729231f879805e2
Mark Channer <markchanner@gmail.com>
2015-10-21 20:15:20 +0100

    No longer compares emoticons with the sameAs() method. Now compares Strings that represent each type

--- app/src/main/java/com/markchanner/mscprojectprototype02/GameView.java
+++ app/src/main/java/com/markchanner/mscprojectprototype02/GameView.java
@@ -9,6 +9,9 @@ import android.graphics.Paint;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -26,11 +29,13 @@ public class GameView extends View {
     private Bitmap embarrassedBitmap;
     private Bitmap surprisedBitmap;
     private Bitmap upsetBitmap;
+    private Bitmap emptyBitmap;
     private int emoticonWidth;
     private int emoticonHeight;
-    private final int ROW = 8;
-    private final int COL = 7;
-    private Emoticon[][] emoticonArray = new Emoticon[ROW][COL];
+    private final int NUM_ROWS = 8;
+    private final int NUM_COLS = 8;
+    private Emoticon[][] emoticonArray;
+    private MatchFinder matchFinder;
     private static final int ZERO = 0;
     private static final int ONE = 1;
     private int[] userSelection01 = new int[2];
@@ -43,7 +48,33 @@ public class GameView extends View {
         context.getResources();
         backgroundColour = new Paint();
         gridLineColour = new Paint();
+        emoticonArray = new Emoticon[NUM_ROWS][NUM_COLS];
+        matchFinder = new MatchFinder(); /** Consider initializing in GameActivity and passing to GameViewConstructor, like Java version */
         resetUserSelections();
+    }
+
+    public int getRows() {
+        return NUM_ROWS;
+    }
+
+    public int getCols() {
+        return NUM_COLS;
+    }
+
+    public Emoticon[][] getEmoticonArray() {
+        if (emoticonArray == null) {
+            throw new NullPointerException(); /* Exceptions need work */
+        } else {
+            return emoticonArray;
+        }
+    }
+
+    private void resetUserSelections() {
+        firstSelectionMade = false;
+        userSelection01[ZERO] = -1;
+        userSelection01[ONE] = -1;
+        userSelection02[ZERO] = -1;
+        userSelection02[ONE] = -1;
     }
 
     @Override
@@ -51,14 +82,17 @@ public class GameView extends View {
         super.onSizeChanged(w, h, oldW, oldH);
         screenWidth = w;
         screenHeight = h;
-        emoticonWidth = screenWidth / 8;
-        emoticonHeight = screenHeight / 7;
+        emoticonWidth = screenWidth / NUM_ROWS;
+        emoticonHeight = screenHeight / NUM_COLS;
         createEmoticons();
     }
-
+    /* Split into scaleBitmaps() and createEmoticons() */
     protected void createEmoticons() {
         Bitmap temp;
         // Retrieves graphics from drawable, scales down, then assigns to Bitmap object
+        temp = BitmapFactory.decodeResource(context.getResources(), R.drawable.empty_tile);
+        emptyBitmap = Bitmap.createScaledBitmap(temp, emoticonWidth, emoticonHeight, false);
+
         temp = BitmapFactory.decodeResource(context.getResources(), R.drawable.angry);
         angryBitmap = Bitmap.createScaledBitmap(temp, emoticonWidth, emoticonHeight, false);
 
@@ -74,20 +108,20 @@ public class GameView extends View {
         temp = BitmapFactory.decodeResource(context.getResources(), R.drawable.upset);
         upsetBitmap = Bitmap.createScaledBitmap(temp, emoticonWidth, emoticonHeight, false);
 
-        // Populates array with emoticon objects with randomly set bitmaps for emotion
+        // Populates array with random emoticon varieties
         Emoticon newEmoticon;
-        for (int rowID = 0; rowID < 8; rowID++) {
-            for (int columnID = 0; columnID < 7; columnID++) {
-                // While the randomly generated Bitmap would mean 3 of the same
-                // type in a row keep randomly generating a different Bitmap
+        for (int rowID = 0; rowID < NUM_ROWS; rowID++) {
+            for (int columnID = 0; columnID < NUM_COLS; columnID++) {
+                // If the randomly generated emoticon would mean 3 of the same
+                // type in a row keep randomly generating a different emoticon
                 do {
                     newEmoticon = generateRandomEmoticon(rowID, columnID);
-                } while ((rowID >= 2 && (newEmoticon.getBitmap()
-                        .sameAs(emoticonArray[rowID - 1][columnID].getBitmap()) &&
-                        newEmoticon.getBitmap().sameAs(emoticonArray[rowID - 2][columnID].getBitmap()))) ||
+                } while ((rowID >= 2 && (newEmoticon.getFace()
+                        .equals(emoticonArray[rowID - 1][columnID].getFace()) &&
+                        newEmoticon.getFace().equals(emoticonArray[rowID - 2][columnID].getFace()))) ||
                         (columnID >= 2 &&
-                                (newEmoticon.getBitmap().sameAs(emoticonArray[rowID][columnID - 1].getBitmap()) &&
-                                        newEmoticon.getBitmap().sameAs(emoticonArray[rowID][columnID - 2].getBitmap()))));
+                                (newEmoticon.getFace().equals(emoticonArray[rowID][columnID - 1].getFace()) &&
+                                        newEmoticon.getFace().equals(emoticonArray[rowID][columnID - 2].getFace()))));
 
                 emoticonArray[rowID][columnID] = newEmoticon;
             }
@@ -101,19 +135,19 @@ public class GameView extends View {
         int value = random.nextInt(5);
         switch (value) {
             case 0:
-                emoticon = new Emoticon(angryBitmap, rowID, columnID);
+                emoticon = new AngryEmoticon(angryBitmap, rowID, columnID);
                 break;
             case 1:
-                emoticon = new Emoticon(delightedBitmap, rowID, columnID);
+                emoticon = new DelightedEmoticon(delightedBitmap, rowID, columnID);
                 break;
             case 2:
-                emoticon = new Emoticon(embarrassedBitmap, rowID, columnID);
+                emoticon = new EmbarrassedEmoticon(embarrassedBitmap, rowID, columnID);
                 break;
             case 3:
-                emoticon = new Emoticon(surprisedBitmap, rowID, columnID);
+                emoticon = new SurprisedEmoticon(surprisedBitmap, rowID, columnID);
                 break;
             case 4:
-                emoticon = new Emoticon(upsetBitmap, rowID, columnID);
+                emoticon = new UpsetEmoticon(upsetBitmap, rowID, columnID);
                 break;
             default:
                 break;
@@ -131,13 +165,13 @@ public class GameView extends View {
         // Draws grid lines within rectangle
         gridLineColour.setStrokeWidth(2f);
         gridLineColour.setColor(Color.BLACK);
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < NUM_ROWS; i++) {
             canvas.drawLine(0, i * emoticonHeight, getWidth(), i * emoticonHeight, gridLineColour);
             canvas.drawLine(i * emoticonWidth, 0, i * emoticonWidth, getHeight(), gridLineColour);
         }
         // Draws emoticonList on canvas
-        for (int row = 0; row < 8; row++) {
-            for (int col = 0; col < 7; col++) {
+        for (int row = 0; row < NUM_ROWS; row++) {
+            for (int col = 0; col < NUM_COLS; col++) {
                 Emoticon e = emoticonArray[row][col];
                 canvas.drawBitmap(e.getBitmap(), row * emoticonWidth, col * emoticonHeight, null);
             }
@@ -206,7 +240,7 @@ public class GameView extends View {
     private void compareTileContents() {
         if (differentPieceTypes()) {
             swapPieces();
-            //findMatches();
+            findMatches();
         } else {
             /* Both selections are of the same Tile. Reset selections */
         }
@@ -214,9 +248,8 @@ public class GameView extends View {
     }
 
     private boolean differentPieceTypes() {
-        /* Uses sameAs() method from graphics.Bitmap */
-        return (!(emoticonArray[userSelection01[ZERO]][userSelection01[ONE]].getBitmap()
-                .sameAs(emoticonArray[userSelection02[ZERO]][userSelection02[ONE]].getBitmap())));
+        return (!(emoticonArray[userSelection01[ZERO]][userSelection01[ONE]].getFace()
+                .equals(emoticonArray[userSelection02[ZERO]][userSelection02[ONE]].getFace())));
     }
 
     private void swapPieces() {
@@ -225,11 +258,83 @@ public class GameView extends View {
         emoticonArray[userSelection02[ZERO]][userSelection02[ONE]] = tempEmoticon;
     }
 
-    private void resetUserSelections() {
-        firstSelectionMade = false;
-        userSelection01[ZERO] = -1;
-        userSelection01[ONE] = -1;
-        userSelection02[ZERO] = -1;
-        userSelection02[ONE] = -1;
+    private void findMatches() {
+        ArrayList<LinkedList<Emoticon>> matchingColumns = matchFinder.findMatchingColumns(this);
+        ArrayList<LinkedList<Emoticon>> matchingRows = matchFinder.findMatchingRows(this);
+        if (matchesFound(matchingColumns, matchingRows)) {
+            //updateBoard(matchingColumns, matchingRows);
+        } else {
+            // The swap did not yield a match, so pieces swapped back to previous position
+            swapPieces();
+        }
+    }
+
+    private boolean matchesFound(ArrayList<LinkedList<Emoticon>> matchingColumns, ArrayList<LinkedList<Emoticon>> matchingRows) {
+        return (!(matchingColumns.isEmpty() && matchingRows.isEmpty()));
+    }
+
+    private void updateBoard(ArrayList<LinkedList<Emoticon>> matchingColumns, ArrayList<LinkedList<Emoticon>> matchingRows) {
+        do {
+            //giveReward(matchingColumns, matchingRows);
+            removeFromBoard(matchingColumns);
+            removeFromBoard(matchingRows);
+            shiftIconsDown();
+            insertNewIcons();
+            matchingColumns = matchFinder.findMatchingColumns(this);
+            matchingRows = matchFinder.findMatchingRows(this);
+        } while (matchesFound(matchingColumns, matchingRows));
+    }
+
+    private void giveReward(ArrayList<LinkedList<Emoticon>> matchingColumns, ArrayList<LinkedList<Emoticon>> matchingRows) {
+        for (LinkedList<Emoticon> matchingColumn : matchingColumns) {
+            //System.out.println(matchingColumn.getFirst().getFace());
+        }
+        for (LinkedList<Emoticon> matchingRow : matchingRows) {
+            //System.out.println(matchingRow.getFirst().getFace());
+        }
+        //printList("Matching columns:", matchingColumns);
+        //printList("Matching rows:", matchingRows);
+    }
+
+    private void removeFromBoard(ArrayList<LinkedList<Emoticon>> matches) {
+        for (List<Emoticon> rowList : matches) {
+            for (Emoticon e : rowList) {
+                int theRow = e.getRow();
+                int theCol = e.getColumn();
+                if (!(emoticonArray[theRow][theCol].getFace().equals("EMPTY"))) {
+                    emoticonArray[theRow][theCol] = new EmptyEmoticon(emptyBitmap,theRow, theCol);
+                }
+            }
+        }
+    }
+
+    private void shiftIconsDown() {
+        for (int col = 0; col < NUM_COLS; col++) {
+            for (int row = (NUM_ROWS - 1); row >= 0; row--) {
+                if (emoticonArray[row][col].getFace().equals("EMPTY")) {
+                    // get any pieces higher up the column and, if found, plug hole with it
+                    int tempRow = row;
+                    while ((tempRow >= 0) && (emoticonArray[tempRow][col].getFace().equals("EMPTY"))) {
+                        tempRow--;
+                    }
+                    if (tempRow >= 0) {
+                        Emoticon e = emoticonArray[tempRow][col];
+                        emoticonArray[row][col] = e;
+                        /* sets previous tile to be empty */
+                        emoticonArray[tempRow][col] = new EmptyEmoticon(emptyBitmap, row, col);
+                    }
+                }
+            }
+        }
+    }
+
+    private void insertNewIcons() {
+        for (int row = 0; row < NUM_ROWS; row++) {
+            for (int col = 0; col < NUM_COLS; col++) {
+                if (emoticonArray[row][col].getFace().equals("EMPTY")) {
+                    emoticonArray[row][col] = generateRandomEmoticon(row, col);
+                }
+            }
+        }
     }
 }
