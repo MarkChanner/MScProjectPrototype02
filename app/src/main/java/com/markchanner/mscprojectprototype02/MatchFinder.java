0d8675e9051556cf12a424525f138c7e53b0c400
Mark Channer <markchanner@gmail.com>
2015-10-21 20:15:20 +0100

    Adds lengthy description of findRows class

--- app/src/main/java/com/markchanner/mscprojectprototype02/MatchFinder.java
+++ app/src/main/java/com/markchanner/mscprojectprototype02/MatchFinder.java
@@ -10,51 +10,81 @@ public class MatchFinder {
 
     public ArrayList<LinkedList<Emoticon>> findMatchingColumns(GameView gameView) {
         Emoticon[][] emoticonArray = gameView.getEmoticonArray();
-        int rowSize = gameView.getRows();
-        int colSize = gameView.getCols();
+        int numRows = gameView.getRows();
+        int numCols = gameView.getCols();
 
-        LinkedList<Emoticon> consecutiveEmoticons = new LinkedList<>();
+        LinkedList<Emoticon> matchingEmoticons = new LinkedList<>();
         ArrayList<LinkedList<Emoticon>> bigList = new ArrayList<>();
         Emoticon emoticon;
-        for (int col = 0; col < colSize; col++) {
-            consecutiveEmoticons.add(emoticonArray[colSize - 1][col]);
+        for (int col = 0; col < numCols; col++) {
+            matchingEmoticons.add(emoticonArray[numRows - 1][col]);
 
-            for (int row = (rowSize - 2); row >= 0; row--) {
+            for (int row = (numRows - 2); row >= 0; row--) {
                 emoticon = emoticonArray[row][col];
-                if (!emoticon.getBitmap().sameAs(consecutiveEmoticons.getLast().getBitmap())) {
-                    examineList(consecutiveEmoticons, bigList);
-                    consecutiveEmoticons = new LinkedList<>();
+                if (!emoticon.getFace().equals(matchingEmoticons.getLast().getFace())) {
+                    checkForThreeOrMoreMatches(matchingEmoticons, bigList);
+                    matchingEmoticons = new LinkedList<>();
                 }
-                consecutiveEmoticons.add(emoticon);
+                matchingEmoticons.add(emoticon);
                 if (row == 0) {
-                    examineList(consecutiveEmoticons, bigList);
-                    consecutiveEmoticons = new LinkedList<>();
+                    checkForThreeOrMoreMatches(matchingEmoticons, bigList);
+                    matchingEmoticons = new LinkedList<>();
                 }
             }
         }
         return bigList;
     }
 
+    /**
+     * Starts scanning the board from the bottom row. Checks columns left to right in
+     * that row, then moves up to the row above. This is so that matching emoticons
+     * will be removed from the bottom of the board up.
+     *
+     * @param gameView the View of the game
+     * @return An ArrayList containing 1 or more LinkedLists of matching emoticons
+     */
     public ArrayList<LinkedList<Emoticon>> findMatchingRows(GameView gameView) {
         Emoticon[][] emoticonArray = gameView.getEmoticonArray();
-        int rowSize = gameView.getRows();
-        int colSize = gameView.getCols();
+        int numRows = gameView.getRows();
+        int numCols = gameView.getCols();
         LinkedList<Emoticon> consecutiveEmoticons = new LinkedList<>();
         ArrayList<LinkedList<Emoticon>> bigList = new ArrayList<>();
         Emoticon emoticon;
 
-        for (int row = (rowSize - 1); row >= 0; row--) {
+        // Starts at bottom-left of board
+        for (int row = (numRows - 1); row >= 0; row--) {
+            // Adds first emoticon in the row to consecutiveEmoticons
+            // list in preparation to begin the below algorithm
             consecutiveEmoticons.add(emoticonArray[row][0]);
-
-            for (int col = 1; col < colSize; col++) {
+            /*
+             * Unless one of the below two conditions are met, adds current tile's
+             * emoticon to consecutiveEmoticons list, then proceeds to next tile.
+             *
+             * Condition 1:
+             * If emoticon at current tile does NOT match emoticon most recently added
+             * to consecutiveEmoticons list (this will be the majority of the time),
+             * calls checkForThreeOrMoreMatches, which checks if consecutiveEmoticons
+             * contains 3 or more emoticons, which would mean that at least 3 of the last tiles
+             * contained matching emoticons and need to be dealt with. This is handled
+             * by adding the consecutiveEmoticons list to bigList, which will be returned
+             * to the GameView when the whole view has been scanned to modify the view accordingly.
+             *
+             * Condition 2:
+             * Having checked Condition 1 (and handled if met), add current tile's emoticon
+             * to consecutiveEmoticons and call checkForThreeOrMoreMatches again. As this
+             * is now the end of the current row, creates a new consecutiveEmoticons list and
+             * either moves up a row, or if on the top row, returns bigList and exits
+             */
+            for (int col = 1; col < numCols; col++) {
                 emoticon = emoticonArray[row][col];
-                if (!emoticon.getBitmap().sameAs(consecutiveEmoticons.getLast().getBitmap())) {
-                    examineList(consecutiveEmoticons, bigList);
+                if (!emoticon.getFace().equals(consecutiveEmoticons.getLast().getFace())) {
+                    checkForThreeOrMoreMatches(consecutiveEmoticons, bigList);
                     consecutiveEmoticons = new LinkedList<>();
                 }
                 consecutiveEmoticons.add(emoticon);
-                if (col == colSize - 1) {
-                    examineList(consecutiveEmoticons, bigList);
+
+                if (col == numCols - 1) {
+                    checkForThreeOrMoreMatches(consecutiveEmoticons, bigList);
                     consecutiveEmoticons = new LinkedList<>();
                 }
             }
@@ -62,7 +92,10 @@ public class MatchFinder {
         return bigList;
     }
 
-    private void examineList(LinkedList<Emoticon> consecutiveEmoticons, ArrayList<LinkedList<Emoticon>> bigList) {
+    private void checkForThreeOrMoreMatches(LinkedList<Emoticon> consecutiveEmoticons, ArrayList<LinkedList<Emoticon>> bigList) {
+        // If the size of the list is 3 or more, this means
+        // we have at least 3 matching emoticons, and these
+        // will be added to bigList to deal with after
         if (consecutiveEmoticons.size() >= 3) {
             bigList.add(consecutiveEmoticons);
         }
