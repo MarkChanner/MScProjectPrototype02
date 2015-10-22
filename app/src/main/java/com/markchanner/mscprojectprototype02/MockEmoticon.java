4311939cd1890674ae846a5d6724689c166caea1
Mark Channer <markchanner@gmail.com>
2015-10-22 14:09:09 +0100

    Adds MockEmoticon for testing

--- app/src/main/java/com/markchanner/mscprojectprototype02/MockEmoticon.java
+++ app/src/main/java/com/markchanner/mscprojectprototype02/MockEmoticon.java
@@ -1,7 +1,25 @@
 package com.markchanner.mscprojectprototype02;
 
+import android.graphics.Bitmap;
+
+
 /**
- * Created by Mark on 22/10/2015.
+ * This game piece differs from others in that it takes a constructor which allows
+ * its type to be set. This is used in the BoardPopulatorMock to populate the board
+ * with ******************* that will not lead to a match and cause difficulties with
+ * running tests
+ *
+ * @author Mark Channer for Birkbeck MSc Computer Science project
  */
-public class MockEmoticon {
+public class MockEmoticon extends AbstractEmoticon {
+
+    public MockEmoticon(Bitmap b, String value) {
+        super(b, value);
+    }
+
+    @Override
+    public Emoticon retrieveEmoticon() {
+        return (this);
+    }
+
 }
