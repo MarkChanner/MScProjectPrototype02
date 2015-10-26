e01070cc3f5e468c06f42d42a8ce469a25fecc03
Mark Channer <markchanner@gmail.com>
2015-10-26 15:43:03 +0000

    Adds MediaPlayer to stram game music

--- app/src/main/java/com/markchanner/mscprojectprototype02/GameActivity.java
+++ app/src/main/java/com/markchanner/mscprojectprototype02/GameActivity.java
@@ -1,15 +1,21 @@
 package com.markchanner.mscprojectprototype02;
 
 import android.app.Activity;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
+import java.io.IOException;
+
 /**
- *
  * @author Mark Channer for Birkbeck MSc Computer Science project
  */
 public class GameActivity extends Activity {
+
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +27,41 @@ public class GameActivity extends Activity {
         /** removed MatchFinderImpl parameter */
         GameView view = new GameView(this, new BoardPopulatorImpl());
         setContentView(view);
+
+        mediaPlayer = new MediaPlayer();
+        try {
+            AssetManager assetManager = getAssets();
+            AssetFileDescriptor musicDescriptor = assetManager.openFd("shroom_ridge.ogg");  // Temporary music
+            mediaPlayer.setDataSource(musicDescriptor.getFileDescriptor(),
+                    musicDescriptor.getStartOffset(), musicDescriptor.getLength());
+            mediaPlayer.prepare();
+            mediaPlayer.setLooping(true);
+        } catch (IOException e) {
+            mediaPlayer = null;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mediaPlayer != null) {
+            mediaPlayer.start();
+        }
+    }
+
+    /**
+     * Consider resources to release here. Also consider instantiating
+     * SoundPool in onCreate()
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mediaPlayer != null) {
+            mediaPlayer.pause();
+            if (isFinishing()) {
+                mediaPlayer.stop();
+                mediaPlayer.release();
+            }
+        }
     }
 }
